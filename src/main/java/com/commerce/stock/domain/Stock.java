da4817424db2a753ea50a61411f1a2a691e62faf
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2026-01-06 18:29:17 +0900

    feat: 재고 도메인 엔티티 추가 (#23)

---
+++ src/main/java/com/commerce/stock/domain/Stock.java
@@ -0,0 +1,33 @@
+package com.commerce.stock.domain;
+
+import com.commerce.product.domain.Product;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Stock {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_id", nullable = false, unique = true)
+	private Product product;
+
+	@Column(nullable = false)
+	private int quantity;
+
+}
