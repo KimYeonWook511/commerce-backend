3fbbb2cba2132e9e2d004f422ebc8f6a63e2d869
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2026-01-06 18:35:37 +0900

    feat: 주문 도메인 엔티티 추가 (#24)

---
+++ src/main/java/com/commerce/order/domain/Order.java
@@ -0,0 +1,41 @@
+package com.commerce.order.domain;
+
+import com.commerce.member.domain.Member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tbl_order")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Order {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	@Column(nullable = false)
+	private int totalPrice;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private OrderStatus status;
+
+}
