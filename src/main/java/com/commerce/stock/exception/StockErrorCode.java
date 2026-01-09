dd551821a3ea1d63e18426744079d81015f18f4c
YeonWook Kim <102712607+KimYeonWook511@users.noreply.github.com>
2026-01-09 17:19:50 +0900

    feat: 재고 차감 동기화 로직 구현 (Optimistic Lock) (#31)
    * feat: Optimistic Lock 기반 재고 차감 로직 추가
    
    * test: 재고 차감 동시성 테스트 보강

--- src/main/java/com/commerce/stock/exception/StockErrorCode.java
+++ src/main/java/com/commerce/stock/exception/StockErrorCode.java
@@ -7,6 +7,7 @@ import com.commerce.common.exception.ErrorCode;
 public enum StockErrorCode implements ErrorCode {
 	STOCK_NOT_FOUND(HttpStatus.NOT_FOUND, "STOCK-404", "재고 정보를 찾을 수 없습니다"),
 	OUT_OF_STOCK(HttpStatus.CONFLICT, "STOCK-409", "재고가 부족합니다"),
+	OPTIMISTIC_LOCK_FAILED(HttpStatus.CONFLICT, "STOCK-409-1", "재고 차감 재시도에 실패했습니다"),
 	INVALID_QUANTITY(HttpStatus.BAD_REQUEST, "STOCK-400", "차감 수량이 올바르지 않습니다");
 
 	private final HttpStatus status;
