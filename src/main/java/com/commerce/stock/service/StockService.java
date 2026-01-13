e7a21c42e1da54af0013d1ab75aa7810e441720f
KimYeonWook511 <kimyeonwook511@gmail.com>
2026-01-13 21:52:54 +0900

    refactor: 낙관적 락 로직을 StockService로 통합

--- src/main/java/com/commerce/stock/service/StockService.java
+++ src/main/java/com/commerce/stock/service/StockService.java
@@ -1,5 +1,6 @@
 package com.commerce.stock.service;
 
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Propagation;
@@ -20,6 +21,8 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class StockService {
+
+	private static final int MAX_RETRY = 5;
 
 	private final StockRepository stockRepository;
 	private final TransactionTemplate transactionTemplate;
@@ -57,6 +60,21 @@ public class StockService {
 			decreaseWithNewTransaction(productId, quantity);
 		} finally {
 			reentrantLock.unlock();
+		}
+	}
+
+	@Transactional(propagation = Propagation.NOT_SUPPORTED)
+	public void decreaseWithOptimisticLock(Long productId, int quantity) {
+		// AOP로도 가능할듯
+		for (int attempt = 1; attempt <= MAX_RETRY; attempt++) {
+			try {
+				decreaseWithNewTransaction(productId, quantity);
+				break;
+			} catch (OptimisticLockingFailureException ex) {
+				if (attempt == MAX_RETRY) {
+					throw new StockException(StockErrorCode.OPTIMISTIC_LOCK_FAILED);
+				}
+			}
 		}
 	}
 
