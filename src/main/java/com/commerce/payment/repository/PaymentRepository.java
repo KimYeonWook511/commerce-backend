e404b6c090ca47691305f483c18b4d8b25166ccc
KimYeonWook511 <kimyeonwook511@gmail.com>
2026-01-27 17:45:43 +0900

    feat: 결제 레포지토리 조회 기능 추가

---
+++ src/main/java/com/commerce/payment/repository/PaymentRepository.java
@@ -0,0 +1,12 @@
+package com.commerce.payment.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.commerce.payment.domain.Payment;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+	Optional<Payment> findByOrderId(Long orderId);
+}
