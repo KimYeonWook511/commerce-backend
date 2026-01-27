0360c854c5ab2fb99c87c4af7c71076d23b81dd0
KimYeonWook511 <kimyeonwook511@gmail.com>
2026-01-27 17:54:41 +0900

    feat: 결제 서비스 추가

--- src/main/java/com/commerce/payment/exception/PaymentErrorCode.java
+++ src/main/java/com/commerce/payment/exception/PaymentErrorCode.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import com.commerce.common.exception.ErrorCode;
 
 public enum PaymentErrorCode implements ErrorCode {
+	PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "PAYMENT-404-1", "결제를 찾을 수 없습니다"),
 	PAYMENT_STATUS_NOT_ALLOWED(HttpStatus.CONFLICT, "PAYMENT-409-1", "결제 상태 변경이 허용되지 않습니다");
 
 	private final HttpStatus status;
