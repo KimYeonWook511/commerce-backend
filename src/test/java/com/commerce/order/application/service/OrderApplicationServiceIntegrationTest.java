0ff31e0c46dc5e007a18aea77853ce324cba71aa
KimYeonWook511 <kimyeonwook511@gmail.com>
2026-06-14 17:02:05 +0900

    refactor: 주문 통합 테스트의 불필요한 import를 제거한다

--- src/test/java/com/commerce/order/application/service/OrderApplicationServiceIntegrationTest.java
+++ src/test/java/com/commerce/order/application/service/OrderApplicationServiceIntegrationTest.java
@@ -18,8 +18,6 @@ import org.springframework.test.context.DynamicPropertySource;
 
 import com.commerce.member.domain.Member;
 import com.commerce.member.infrastructure.persistence.support.MemberPersistenceTestSupport;
-import com.commerce.order.application.service.OrderCancelService;
-import com.commerce.order.application.service.OrderCreateService;
 import com.commerce.order.application.command.OrderCreateCommand;
 import com.commerce.order.application.command.OrderCreateItem;
 import com.commerce.order.application.result.OrderCreateResult;
