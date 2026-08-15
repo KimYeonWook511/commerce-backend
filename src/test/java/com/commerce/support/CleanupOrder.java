0958223104327558cefb0e840ca24f0f45c5ef67
KimYeonWook511 <kimyeonwook511@gmail.com>
2026-08-15 11:27:33 +0900

    test: 테스트 정리 순서에서 legacy 결제 항목을 제거한다

--- src/test/java/com/commerce/support/CleanupOrder.java
+++ src/test/java/com/commerce/support/CleanupOrder.java
@@ -5,8 +5,6 @@ public enum CleanupOrder {
 	// 환불이 결제를 식별자로 가리킨다. 외래 키는 없지만 순서를 정해 두어야 읽는 사람이 관계를 안다.
 	REFUND(2),
 	PAYMENT(3),
-	LEGACY_PAYMENT_RESERVATION(5),
-	LEGACY_PAYMENT(10),
 	OUTBOX(20),
 	CART(25),
 	ORDER(30),
